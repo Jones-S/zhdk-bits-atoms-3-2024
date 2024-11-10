034ccd327d759ffb15637f24444f377ff2fdc22e
Jonas <git@jonasscheiwiller.ch>
2024-11-11 00:36:51 +0100

    feat: add starter for bar chart

---
+++ examples/d3-bar-chart/main.js
@@ -0,0 +1,16 @@
+async function fetchData() {
+  const url = "./data.json"; // data from https://opendata.swiss/en/dataset/treibhausgasemissionen-im-kanton-zurich
+  let response = await fetch(url);
+
+  if (response.ok) {
+    // if HTTP-status is 200-299
+    // get the response body (the method explained below)
+    let json = await response.json();
+    console.log("Finally received the response:");
+    console.log("Response: ", json);
+  } else {
+    alert("HTTP-Error: " + response.status);
+  }
+}
+
+fetchData();
