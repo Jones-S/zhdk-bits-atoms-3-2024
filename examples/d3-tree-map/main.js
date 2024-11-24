02208a3b8afcf072e36663d7bd04c3279391c6d3
Jonas <git@jonasscheiwiller.ch>
2024-11-24 17:45:53 +0100

    feat: add second data set and switch with transitions

--- examples/d3-tree-map/main.js
+++ examples/d3-tree-map/main.js
@@ -6,10 +6,7 @@ console.log("Displaying simple bar chart");
 const width = 800;
 const height = 800;
 
-const marginTop = 20;
-const marginRight = 20;
-const marginBottom = 30;
-const marginLeft = 40;
+let svg;
 
 // get all the names for the items without children in a flat array
 function getNames(node) {
@@ -26,8 +23,8 @@ function getNames(node) {
   return names;
 }
 
-async function fetchData() {
-  const url = "./data.json"; // data from https://opendata.swiss/en/dataset/treibhausgasemissionen-im-kanton-zurich
+async function fetchData(dataSet) {
+  const url = `./${dataSet}`; // data from https://opendata.swiss/en/dataset/treibhausgasemissionen-im-kanton-zurich
   let response = await fetch(url);
 
   if (response.ok) {
@@ -59,6 +56,24 @@ Id.prototype.toString = function () {
   return "url(" + this.href + ")";
 };
 
+function addEventListeners() {
+  const form = document.querySelector(".form");
+
+  form.addEventListener("change", (event) => {
+    if (event.target.name === "options") {
+      console.log(`Selected option: ${event.target.value}`);
+      let path = "data.json";
+
+      if (event.target.value === "ch") {
+        path = "data-ch.json";
+      }
+
+      // refetch the data with the new path
+      fetchData(path);
+    }
+  });
+}
+
 function drawChart(data) {
   const tileNames = getNames(data);
   console.log("tileNames: ", tileNames);
@@ -67,7 +82,7 @@ function drawChart(data) {
   const color = d3.scaleOrdinal(
     tileNames, // names:  ["Livestock","Crops (excl. Feeds)","Forests","Shrub","Urban","Freshwater","Glaciers","Barren land"]
     [
-      "#ABB166",
+      "#FFB282",
       "#91BF61",
       "#34A60B",
       "#FFE187",
@@ -97,38 +112,32 @@ function drawChart(data) {
   );
 
   // Create the SVG container.
-  const svg = d3
-    .create("svg")
-    .attr("viewBox", [0, 0, width, height])
-    .attr("width", width)
-    .attr("height", height);
+  if (!svg) {
+    svg = d3
+      .create("svg")
+      .attr("viewBox", [0, 0, width, height])
+      .attr("width", width)
+      .attr("height", height);
+  }
 
   // Add a cell for each leaf of the hierarchy.
-  const leaf = svg
-    .selectAll("g")
-    .data(root.leaves())
-    .join("g")
+  const leaf = svg.selectAll("g").data(root.leaves(), (d) => d.data.name); // make sure to use the name as key, otherwise the update will not work
+
+  // define enter (instead of using .join, which would also handle update and exit)
+  const leafEnter = leaf
+    .enter()
+    .append("g")
     .attr("transform", (d) => `translate(${d.x0},${d.y0})`);
 
-  // Append a tooltip.
-  const format = d3.format(",d");
-  leaf.append("title").text(
-    (d) =>
-      `${d
-        .ancestors()
-        .reverse()
-        .map((d) => d.data.name)
-        .join(".")}\n${format(d.value)}`
-  );
-
   // Append a color rectangle.
-  leaf
+  leafEnter
     .append("rect")
     .attr("id", (d) => (d.leafUid = uid("leaf")).id)
     .attr("fill", (d) => {
       return color(d.data.name);
     })
     .attr("stroke-width", 1)
+    // depending on the level of the hierarchy, change the stroke color
     .attr("stroke", (d) => {
       if (d.depth >= 3) {
         return "#5F8E79";
@@ -136,9 +145,11 @@ function drawChart(data) {
         return "#5A3D3D";
       }
     })
-    .attr("fill-opacity", 0.6)
+    .attr("fill-opacity", 0)
     .attr("width", (d) => d.x1 - d.x0)
     .attr("height", (d) => d.y1 - d.y0)
+    // and also add event listeners for mouseover, mousemove and mouseout
+    // must be added to the rect otherwise d will not be refering to the data
     .on("mouseover", (event, d) => {
       console.log("d: ", d);
       tooltip.style("visibility", "visible");
@@ -151,17 +162,13 @@ function drawChart(data) {
     })
     .on("mouseout", () => {
       tooltip.style("visibility", "hidden");
-    });
+    })
+    .transition()
+    .duration(750)
+    .attr("fill-opacity", 0.6)
+    .attr("stroke-opacity", 0.6);
 
-  // Append a clipPath to ensure text does not overflow.
-  leaf
-    .append("clipPath")
-    .attr("id", (d) => (d.leafUid = uid("leaf")).id)
-    .append("use")
-    .attr("xlink:href", (d) => d.leafUid.href);
-
-  // Append multiline text. The last line shows the value and has a specific formatting.
-  leaf
+  leafEnter
     .append("text")
     .attr("clip-path", (d) => d.clipUid)
     .selectAll("tspan")
@@ -179,12 +186,43 @@ function drawChart(data) {
     .attr("fill-opacity", (d, i, nodes) =>
       i === nodes.length - 1 ? 0.7 : null
     )
+    .text((d) => d)
+    .transition()
+    .duration(750);
+
+  // Handle update selection (existing data).
+  leaf
+    .transition()
+    .duration(750) // Smooth transition for updates.
+    .attr("transform", (d) => `translate(${d.x0},${d.y0})`);
+
+  // don't chain methods with transition above to wait until the first transition is finished
+  leaf
+    .select("rect")
+    .transition()
+    .duration(750)
+    .attr("width", (d) => d.x1 - d.x0)
+    .attr("height", (d) => d.y1 - d.y0);
+
+  leaf
+    .select("text")
+    .selectAll("tspan")
+    .data((d) => {
+      return d.data.name
+        .split(/(?=[A-Z][a-z])|\s+/g)
+        .concat(`${d.data.value.toFixed(2)}%`);
+    })
     .text((d) => d);
 
+  // Handle exit selection (remove data).
+  leaf.exit().remove();
+
+  // append tooltip for mouseover
   let tooltip = d3.select("body").append("div").attr("class", "tooltip");
 
-  console.log("svg.node(): ", svg.node());
   container.append(svg.node());
 }
 
-fetchData();
+// make the document listen for changes on the radiobutton
+addEventListeners();
+fetchData("data.json");
